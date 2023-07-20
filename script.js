2e5cb32a5dd9d17b6b4ce596b03019c077baad04
ermal666 <ermalkk14@gmail.com>
2023-07-20 23:39:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+// var navsidemenu = document.getElementById("sidemenu");
+
+// function openmenu(){
+//   navsidemenu.style.right = "0";
+// }
+
+// function closemenu(){
+//   navsidemenu.style.right = "-200px";
+// }
+
+//////////////////////////////////////////////
+
+//PLAY VIDEO ON HOVER SCRIPT
+// const videos = document.querySelectorAll('.video');
+
+// videos.forEach(video => {
+//   video.addEventListener('mouseenter', () => {
+//     video.play();
+//   });
+  
+//   video.addEventListener('mouseleave', () => {
+//     video.pause();
+//     video.currentTime = 0;
+//   });
+// });
+
